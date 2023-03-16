665ca14edac00973b72b1cd9d123c81e713eff34
isaidakbar <smahmudxojaev@gmail.com>
2023-03-16 14:53:11 +0500

    modifies: api https

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,14 +1,14 @@
 const API = 'b4acff7684fa39b7986123455be7bcbc'
 
 export const getCountries = async ({country, limit = 10}, options) => {
-    const WEATHER_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${country}&limit=${limit}&appid=${API}`
+    const WEATHER_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${country}&limit=${limit}&appid=${API}`
     const response = await fetch(WEATHER_URL, options);
     const data = await response.json();
     return data;
 }
 
 export const getWeather = async ({lat, long}, options) => {
-    const WEATHER_URL = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API}&units=metric`
+    const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API}&units=metric`
     const response = await fetch(WEATHER_URL, options);
     const data = await response.json();
     return data;
